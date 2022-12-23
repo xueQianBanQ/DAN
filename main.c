d9ec3153d1ca79bbf02a5f3998d2d33867153143
xueQianBanQ <115210866+xueQianBanQ@users.noreply.github.com>
2022-12-23 22:15:14 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <REGX52.H>
-	
-void Delay (unsigned int xms)  // 延迟1s
+
+sbit RCK=P3^5;     //RCLK
+sbit SCK=P3^6;     //SRCLK
+sbit SER=P3^4;     //SER
+
+void Delay (unsigned int xms)  // ??1s
 {
 	unsigned char i, j;
 	while(xms--)
 	{
 		i = 2;
-		j = 239;
+		j = 100;//239
 		do
 		{
 			while(--j);
@@ -14,70 +18,99 @@ void Delay (unsigned int xms)  // 延迟1s
 	}
 }
 
-
-void Delay1ms(unsigned int xms)		//@11.0592MHz
+void _74HC595_WriteByte(unsigned char Byte)
 {
-	unsigned char i, j;
-	while(xms--)
-    {
-
-	i = 2;
-	j = 199;
-	do
+	unsigned char i;
+	for (i = 0; i < 8; i++)
 	{
-		while (--j);
-	} while (--i);
+		SER=Byte&(0x80>>i);
+		SCK = 1;
+		SCK = 0;
+	}
+	RCK = 1;
+	RCK = 0;
 }
-}
 
-
-unsigned char Nixietable[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f };
-
-void Nixietube(unsigned char Location, unsigned char Number)
+void MatrixLED_ShowColumn(unsigned char Column, unsigned char Data)
 {
-	Location = 8 - Location;
-	P2_2 = Location % 2;
-	P2_3 = Location / 2 % 2;
-	P2_4 = Location / 2 / 2 % 2;
-	P0 = Nixietable[Number]; 
+	_74HC595_WriteByte(Data);
+	P0 = ~(0x80>>Column);
 	Delay(1);
-	P0 = 0x00;
+	P0 = 0xff;
 }
 
-//int main()
-
+//void main()
 //{
+//	SCK = 0;
+//	RCK = 0;
+
 //	while(1)
 //	{
-//		
-//		Nixietube(1, 1);
-//		Delay1ms(200);
-//		Nixietube(2, 2);
-//		Delay1ms(200);
-//		Nixietube(3, 3);
-//		Delay1ms(200);
+//		MatrixLED_ShowColumn(0, 0xa0);//1010 0000
+//		MatrixLED_ShowColumn(0, 0xff);
 //	}
-//	return 0;
+
+//}
+
+////第一种爱心
+//void main()
+//{
+//	SCK = 0;
+//	RCK = 0;
+
+//	while(1)
+//	{
+//		MatrixLED_ShowColumn(0, 0x38);
+//		MatrixLED_ShowColumn(1, 0x7c);
+//		MatrixLED_ShowColumn(2, 0xfe);
+//		MatrixLED_ShowColumn(3, 0x7f);
+//		MatrixLED_ShowColumn(4, 0x7f);
+//		MatrixLED_ShowColumn(5, 0xfe);
+//        MatrixLED_ShowColumn(6, 0x7c);
+//		MatrixLED_ShowColumn(7, 0x38);
+//	}
+
+//}
+
+////小爱心
+//void main()
+//{
+//	SCK = 0;
+//	RCK = 0;
+
+//	while(1)
+//	{
+//		MatrixLED_ShowColumn(0, 0x04);
+//		MatrixLED_ShowColumn(1, 0x0e);
+//		MatrixLED_ShowColumn(2, 0x07);
+//		MatrixLED_ShowColumn(3, 0x0e);
+//		MatrixLED_ShowColumn(4, 0x04);
+
+//	}
 
 //}
 
 
-int main()
+////两个小爱心
+//void main()
+//{
+//	SCK = 0;
+//	RCK = 0;
 
-{
-	while(1)
-	{
-		
-		Nixietube(1, 5);
-		Nixietube(2, 2);
-		Nixietube(3, 0);
-		Nixietube(4, 1);
-		Nixietube(5, 3);
-		Nixietube(6, 1);
-		Nixietube(7, 4);
-	}
-	return 0;
+//	while(1)
+//	{
+//		MatrixLED_ShowColumn(0, 0x04);
+//		MatrixLED_ShowColumn(1, 0x0e);
+//		MatrixLED_ShowColumn(2, 0x07);
+//		MatrixLED_ShowColumn(3, 0x4e);
+//		MatrixLED_ShowColumn(4, 0xe4);
+//		MatrixLED_ShowColumn(5, 0x70);
+//		MatrixLED_ShowColumn(6, 0xe0);
+//		MatrixLED_ShowColumn(7, 0x40);
 
-}
+//	}
+
+//}
+
 
 
